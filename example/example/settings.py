38f7d10d8702c6522679fd01dd064452fa46e553
devil_shinji <frank.kiibus@gmail.com>
2022-02-17 16:26:39 +0200

    Upgrade progress

--- example/example/settings.py
+++ example/example/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'invoices',
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- tests/test_project/settings.py
+++ tests/test_project/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'test_app',
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
