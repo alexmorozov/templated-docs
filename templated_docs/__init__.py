45c64c0094122196281ce6effab7d76a9d5d8666
devil_shinji <frank.kiibus@redfunction.ee>
2022-09-22 07:57:06 +0300

    Feedback related improvements

--- templated_docs/__init__.py
+++ templated_docs/__init__.py
@@ -54,6 +54,8 @@ def fix_inline_tags(content):
         text = text.replace('<text:s/>', ' ')
         text = text.replace('&apos;', "'")
         text = text.replace('&quot;', '"')
+        text = text.replace('&gt;', '>')
+        text = text.replace('&lt;', '<')
         return re.sub(r'<[^>]+>', '', text)
 
     django_tag_re = r'(\{[\{\%].+?[\%\}]\})'
